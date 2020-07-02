c0fb1c1fea173d84acfbb90a92ecabe637e12c7c
i-am-expert <onlyforgithub2708@gmail.com>
2020-07-02 16:10:46 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+// action creater is a function, here buyCake, which returns an action
+function buyCake() {
+  return {
+    // This action is being returned
+    type: BUY_CAKE,
+    info: "First Redux Action",
+  };
+}
+
+function buyIceCream() {
+  return {
+    // This action is being returned
+    type: BUY_ICECREAM,
+    info: "Second Redux Action",
+  };
+}
+
+// initial state must be an object
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// };
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// reducer is a function which takes previous state and action as arguments, and return new state (doesn't modify prev state, instead returns new state)
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state, // we make copy of state object and change only those properties which are to be changed
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state, // we make copy of state object and change only those properties which are to be changed
+//         numOfIceCreams: state.numOfIceCreams - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, // we make copy of state object and change only those properties which are to be changed
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state, // we make copy of state object and change only those properties which are to be changed
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer); // storing the application state
+console.log("Initial State: ", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated State: ", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
